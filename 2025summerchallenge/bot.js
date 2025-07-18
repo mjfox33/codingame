a2bff5d3a4acee0e4e2ca96f425c41d78dd6db87
Michael and Tammy Fox <michaelandtammyfox@Michaels-iMac.localdomain>
2025-07-18 18:54:35 -0400

    passign wood 3 now

--- 2025summerchallenge/bot.js
+++ 2025summerchallenge/bot.js
@@ -1,36 +1,55 @@
 // Victory Conditions
-// In this league, you will have exactly 1 turn to get both your agents behind 
-// the best of two adjacent tiles behind cover 
-// then shoot the opposing enemy with the least protection from cover.
+// In this league, you will have exactly 1 turn to get both 
+// your agents behind the best of two adjacent tiles behind 
+// cover then shoot the opposing enemy with the least protection 
+// from cover (of the two closest enemies).
 const myId = parseInt(readline()); // Your player id (0 or 1)
 const agentCount = parseInt(readline()); // Total number of agents in the game
 const myAgents = [];
+const myAgentIds = new Set();
 const oppAgents = [];
+const oppAgentIds = new Set();
+const TILE_EMPTY = 0;
+const TILE_LOW_COVER = 1;
+const TILE_HIGH_COVER = 2;
 
 for (let i = 0; i < agentCount; i++) {
-  const currAgent = {};  
   const agentInputs = readline().split(' ');
-  currAgent.agentId = parseInt(agentInputs[0]); // Unique identifier for this agent
-  currAgent.player = parseInt(agentInputs[1]); // Player id of this agent
-  currAgent.shootCooldown = parseInt(agentInputs[2]); // Number of turns between each of this agent's shots
-  currAgent.optimalRange = parseInt(agentInputs[3]); // Maximum manhattan distance for greatest damage output
-  currAgent.soakingPower = parseInt(agentInputs[4]); // Damage output within optimal conditions
-  currAgent.splashBombs = parseInt(agentInputs[5]); // Number of splash bombs this can throw this game
-  agents.push(currAgent);
+  
+  const currAgent = {  
+    agentId: parseInt(agentInputs[0]), // Unique identifier for this agent
+    player: parseInt(agentInputs[1]), // Player id of this agent
+    shootCooldown: parseInt(agentInputs[2]), // Number of turns between each of this agent's shots
+    optimalRange: parseInt(agentInputs[3]), // Maximum manhattan distance for greatest damage output
+    soakingPower: parseInt(agentInputs[4]), // Damage output within optimal conditions
+    splashBombs: parseInt(agentInputs[5]), // Number of splash bombs this can throw this game
+  };
+
+  if (myId == currAgent.player) {
+    myAgentIds.add(currAgent.agentId);
+    myAgents.push(currAgent);
+  } else {
+    oppAgentIds.add(currAgent.agentId);
+    oppAgents.push(currAgent);
+  }
+
 }
 
-const gameMap = {};
+
 const mapInputs = readline().split(' ');
-gameMap.width = parseInt(mapInputs[0]); // Width of the game map
-gameMap.height = parseInt(mapInputs[1]); // Height of the game map
-gameMap.board = [];
+const gameMap = {
+  width: parseInt(mapInputs[0]), // Width of the game map
+  height: parseInt(mapInputs[1]), // Height of the game map
+  board: [],
+};
+
 for (let i = 0; i < gameMap.height; i++) {
   const row = [];
-    const boardInputs = readline().split(' ');
-    for (let j = 0; j < gameMap.width; j++) {
-        const x = parseInt(boardInputs[3*j]);// X coordinate, 0 is left edge
-        const y = parseInt(boardInputs[3*j+1]);// Y coordinate, 0 is top edge
-        const tileType = parseInt(boardInputs[3*j+2]);
+  const boardInputs = readline().split(' ');
+  for (let j = 0; j < gameMap.width; j++) {
+    const x = parseInt(boardInputs[3*j]);// X coordinate, 0 is left edge
+    const y = parseInt(boardInputs[3*j+1]);// Y coordinate, 0 is top edge
+    const tileType = parseInt(boardInputs[3*j+2]);
     row.push(tileType);  
   }
   gameMap.board.push(row);
@@ -38,24 +57,44 @@ for (let i = 0; i < gameMap.height; i++) {
 
 // game loop
 while (true) {
-  const wetestAgent = { id: 0, wetness: -1 };
   const agentCount = parseInt(readline());
   const agents = [];
   for (let i = 0; i < agentCount; i++) {
-    const agent = {};
     const inputs = readline().split(' ');
-    agent.agentId = parseInt(inputs[0]);
-    agent.x = parseInt(inputs[1]);
-    agent.y = parseInt(inputs[2]);
-    agent.cooldown = parseInt(inputs[3]); // Number of turns before this agent can shoot
-    agent.splashBombs = parseInt(inputs[4]);
-    agent.wetness = parseInt(inputs[5]); // Damage (0-100) this agent has taken
+    const agent = {
+      agentId: parseInt(inputs[0]),
+      x: parseInt(inputs[1]),
+      y: parseInt(inputs[2]),
+      cooldown: parseInt(inputs[3]), // Number of turns before this agent can shoot
+      splashBombs: parseInt(inputs[4]),
+      wetness: parseInt(inputs[5]), // Damage (0-100) this agent has taken
+    };
+    
     agents.push(agent);
-    if (agent.wetness > wetestAgent.wetness) {
-      wetestAgent.id = agent.agentId;
-      wetestAgent.wetness = agent.wetness;
+  }
+
+  const moves = [];
+  for (const agent of agents) {
+    if (myAgentIds.has(agent.agentId)) {
+      const xOffset = agent.x == 0 ? 1 : -1;
+      let move = "";
+      if (gameMap.board[agent.y - 1][agent.x + xOffset] > gameMap.board[agent.y + 1][agent.x + xOffset]) {
+        move += `${agent.agentId}; MOVE ${agent.x} ${agent.y - 1};`;
+      } else {
+        move += `${agent.agentId}; MOVE ${agent.x} ${agent.y + 1};`;
+      }
+
+      let targetAgentId = -1;
+      const agentOffset = agent.agentId == 1 ? 2 : 4;
+      const targetOffset = agent.x == 0 ? -1 : 1;
+      const tile1 = gameMap.board[agents[agentOffset].y][agents[agentOffset].x + targetOffset];
+      const tile2 = gameMap.board[agents[agentOffset + 1].y][agents[agentOffset + 1].x + targetOffset];
+      targetAgentId = tile1 < tile2 ? agentOffset + 1 : agentOffset + 2;
+      move += `SHOOT ${targetAgentId}`;
+      moves.push(move);
     }
   }
+  
     
   const myAgentCount = parseInt(readline()); // Number of alive agents controlled by you
   for (let i = 0; i < myAgentCount; i++) {
@@ -65,6 +104,6 @@ while (true) {
 
 
     // One line per agent: <agentId>;<action1;action2;...> actions are "MOVE x y | SHOOT id | THROW x y | HUNKER_DOWN | MESSAGE text"
-    console.log(`SHOOT ${wetestAgent.id}`);
+    console.log(`${moves[i]}`);
   }
 }
